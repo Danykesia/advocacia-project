60e917bef227ae58175d26f50c388ce9e5e85469
danykesia <danielekesia@outlook.com>
2020-01-31 07:27:34 -0300

    create navbar toggle

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,14 @@
 <template>
   <header>
-    <nav id="navbar" class="container navbar">
-      <img href="/" class="navbar_logo" src="../assets/balance.svg" alt="Logo">
-      <ul class="navbar_links">
+    <nav id="navbar" class="navbar">
+      <div class="toggle_mobile">
+        <img href="/" class="navbar_logo" src="../assets/balance.svg" alt="Logo">
+        <button id="toggle" class="navbar_btn" @click="showMenu = !showMenu">
+          <font-awesome-icon icon="bars"/>
+        </button>
+      </div>
+
+      <ul class="navbar_links" :class="{'showMenu': showMenu}">
         <li>
           <router-link to="/" active-class="active" exact>Home</router-link>
         </li>
@@ -25,6 +31,11 @@
 
 <script>
 export default {
+  data() {
+    return {
+      showMenu: false,
+    };
+  },
   mounted() {
     window.onscroll = function scrollFunction() {
       if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 80) {
@@ -33,13 +44,22 @@ export default {
         document.getElementById('navbar').style.background = 'transparent';
       }
     };
+    // document.getElementById('toggle').addEventListener('click', () => {
+    //   const menuLinks = document.querySelector('.navbar_links');
+    //   const { display } = menuLinks.style;
+    //   if (display === 'block') {
+    //     menuLinks.style.display = 'none';
+    //   } else {
+    //     menuLinks.style.display = 'block';
+    //   }
+    // });
   },
 };
 </script>
 
 <style lang="scss" scoped>
 header {
-  position: fixed;
+  position: absolute;
   width: 100%;
   z-index: 1000;
   left: 0;
@@ -48,43 +68,61 @@ header {
 
 .navbar {
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   flex-wrap: nowrap;
   justify-content: space-between;
   align-items: center;
   height: 70px;
   width: 100%;
-  padding-top: .5rem;
-  padding-bottom: .5rem;
+  padding-top: 2rem;
+  // padding-bottom: .5rem;
+
+  &_btn {
+    margin-right: 2rem;
+    background: none;
+    border: none;
+    font-size: 1.8rem;
+    color: rgb(247, 247, 247);
+    background: rgba(107, 107, 107, 0.137);
+    border-radius: 3px;
+    padding: .3rem .6rem;
+    display: flex;
+  }
 
   &_logo {
-    max-height: 100%;
+    margin-left: 2rem;
+    max-height: 50px;
     cursor: pointer;
   }
 
   & ul {
-    display: flex;
+    display: none;
     color: white;
+
+    & li a {
+      margin: 0;
+    }
   }
 
-  &_links li {
-    padding-left: 1rem;
-    padding-right: 1rem;
+  & ul.showMenu {
+    display: block;
+    width: 100%;
+    background: #4e4e4e;
+    margin-top: 2rem;
+    padding: 0;
+  }
 
-    &:last-child {
-      padding-right: 0rem;
-    }
-
-    &:first-child {
-      padding-left: 0rem;
-    }
-
-    & a{
+    & li a {
       color: #ffffff;
       text-transform: uppercase;
       transition: all 0.3s ease 0s;
       letter-spacing: 0.1rem;
       font-family: 'Source Sans Pro', sans-serif;
+      white-space: nowrap;
+      border-top: 1px solid rgba(56, 56, 56, 0.226);
+      justify-content: center;
+      padding: .5rem;
+      display: flex;
 
       &:hover {
         color: #eca944;
@@ -95,11 +133,49 @@ header {
       color: #eca944;
     }
   }
+
+
+.toggle_mobile {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  align-items: center;
 }
 
-        @media (min-width: 768px) {
-          .navbar {
-            height: 80px;
-          }
-        }
+@media (min-width: 991px) {
+  header {
+    position: fixed;
+  }
+
+  .navbar {
+    height: 80px;
+    flex-direction: row;
+    padding-top: 0;
+
+    &_logo {
+      margin-left: 4rem;
+    }
+
+    & ul {
+      margin-right: 4rem;
+    }
+
+    &_links li {
+
+      & a {
+        border: none;
+        margin-right: 4rem;
+      }
+    }
+
+    & ul, ul.showMenu {
+      display: flex;
+      background: none;
+    }
+
+    &_btn {
+      display: none;
+    }
+  }
+}
 </style>
